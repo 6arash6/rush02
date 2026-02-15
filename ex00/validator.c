755850caf85842f62d20a11682702f9d570022d8
𝔸𝕣𝕒𝕤𝕙 <44658653+6arash6@users.noreply.github.com>
2026-02-15 14:02:29 +0100

    Update input validation function to check for digits

--- ex00/validator.c
+++ ex00/validator.c
@@ -1,1 +1,18 @@
 //Input validation - Checks if the input is a valid number (only digits, no negatives, no decimals).
+#include "rush02.h"
+
+int	is_valid(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str || !str[0])
+		return (0);
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}

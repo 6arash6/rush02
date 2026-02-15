af9fce7e5bab591f3733aa265451855493195f87
𝔸𝕣𝕒𝕤𝕙 <44658653+6arash6@users.noreply.github.com>
2026-02-15 14:03:22 +0100

    Add string utility functions to utils.c

--- ex00/utils.c
+++ ex00/utils.c
@@ -1,1 +1,50 @@
 //String utilities - Basic helper functions like strlen, strcmp, strdup, isdigit, putstr.
+#include "rush02.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+char	*ft_strdup(char *str)
+{
+	char	*dup;
+	int		i;
+
+	dup = malloc(ft_strlen(str) + 1);
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		dup[i] = str[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+int	ft_isdigit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+void	ft_putstr(char *str)
+{
+	write(1, str, ft_strlen(str));
+}
